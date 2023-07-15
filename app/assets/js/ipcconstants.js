97fd0d4cad85f3da4db6e6c2011a2092ccd14aa5
enn3 <arturpeshko39@gmail.com>
2023-07-15 19:08:53 +0200

    Added RGBLauncher's client id

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '321a0b0c-cc46-4df0-840a-38fe2a86b798'
 // SEE NOTE ABOVE.
 
 
